4229b36bc79e12c40891f4ba0f80ff53c41d0689
Samin Ahmed Chowdhury <85004796+sam-in07@users.noreply.github.com>
2025-08-23 07:37:42 +0600

    "Commit"

--- lib/profile.dart
+++ lib/profile.dart
@@ -9,32 +9,141 @@ import 'package:flutter/material.dart';
     // TODO: implement build
    //  throw UnimplementedError();
     return Scaffold(
-      body: Column(
+      body:
+      Column(
         children: [
+          Center(
+            child: Center(
+              child: Column(
+                children: [
 
-          SizedBox(
-            height: 100,
-            child: ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return   Container(height: 100 ,
-                  width: 60,
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    shape: BoxShape.circle,
+                  SizedBox(
+                    height: 100,
+                    child: ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 5,
+                      itemBuilder: (context, index) {
+                        return   Container(
+                      //    height: 60 ,
+                          height: MediaQuery.of(context).size.height / 5 - 20 ,
+                          width: MediaQuery.of(context).size.width / 5 - 20 ,
+                          // 5 jon nise 100 padding 1 jon nise 20 kore
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            shape: BoxShape.circle,
+                          ),
+                        ) ;
+                      },
+                      separatorBuilder: (context, index) {
+                        return SizedBox(width: 15,);
+                      },
+                    ),
                   ),
-                ) ;
-              },
-              separatorBuilder: (context, index) {
-                return SizedBox(width: 15,);
-              },
+
+
+                ],
+              ),
             ),
           ),
+          Row(
+            children: [
+              Container(
+                height: 60 ,
+                width: MediaQuery.of(context).size.width / 5,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  shape: BoxShape.circle,
+                ),
+              ),
+              Container(
+                height: 60 ,
+                width: MediaQuery.of(context).size.width / 5,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  shape: BoxShape.circle,
+                ),
+              ),
+              Container(
+                height: 60 ,
+                width: MediaQuery.of(context).size.width / 5,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  shape: BoxShape.circle,
+                ),
+              ),
+              Container(
+                height: 60 ,
+                width: MediaQuery.of(context).size.width / 5,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  shape: BoxShape.circle,
+                ),
+              ),
+              Container(
+                height: 60 ,
+                width: MediaQuery.of(context).size.width / 5,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  shape: BoxShape.circle,
+                ),
+              ),
 
 
+            ],
+          ),
+          // 3* 6 = 18 + 20 = 38 / 3 = 13
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0),
+            child: SizedBox(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  spacing: 6,
+                  children: [
+                     Container(
+                        height: 150 ,
+                       width: MediaQuery.of(context).size.width / 3 - 13 ,
+                       decoration: BoxDecoration(
+                         color: Colors.green,
+                          borderRadius: BorderRadius.circular(20),
+
+                       ),
+                     ),
+                     Container(
+                        height: 140,
+                       width: MediaQuery.of(context).size.width / 3 - 13 ,
+                       decoration: BoxDecoration(
+                         color: Colors.green,
+                          borderRadius: BorderRadius.circular(20),
+
+                       ),
+                     ),
+                     Container(
+                        height: 140,
+                       width: MediaQuery.of(context).size.width / 3 - 13 ,
+                       decoration: BoxDecoration(
+                         color: Colors.green,
+                          borderRadius: BorderRadius.circular(20),
+
+                       ),
+                     ),
+                     Container(
+                        height: 140,
+                       width: MediaQuery.of(context).size.width / 3 - 13 ,
+                       decoration: BoxDecoration(
+                         color: Colors.green,
+                          borderRadius: BorderRadius.circular(20),
+
+                       ),
+                     ),
+                  ],
+                ),
+              ),
+            ),
+          )
         ],
       ),
+
     );
   }
    
